556f12a81784640b03b6a29490e6fe9936305a64
Douglas Benitez <douglasfazion@gmail.com>
2024-01-04 15:24:47 -0300

    feat add prisma and user migrations

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+  @Post()
+  createUser(@Body() user: Prisma.UserCreateInput) {
+    return this.userService.createUser(user);
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(+id);
+  }
+
+  @Get()
+  getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() user: Prisma.UserUpdateInput) {
+    return this.userService.updateUser(+id, user);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.deleteUser(+id);
+  }
+}
